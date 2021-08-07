1a4448b65d286273a9c3892e9b1132b5ace858f9
Wasim Sarhan <wasimAlgahlani@gmail.com>
2021-08-07 14:44:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,40 +2,6 @@
 import random
 from game_data import data
 
-# def check(num, attempts):
-#     """Code"""
-#     while attempts > 0:
-#         print(f"You have {attempts} remaining to guess the number.")
-#         guess = int(input("Make a guess: "))
-#         if guess > num:
-#             print("Too high.")
-#         elif guess < num:
-#             print("Too low.")
-#         else:
-#             print(f"You got it! The answer was {num}.")
-#             break
-#         if guess > num or guess < num and attempts > 1:
-#             print("Guess again.")
-#         attempts -= 1
-#     if attempts == 0:
-#         print(f"You've run out of guesses, you lose, the number was {num}.")
-#
-#
-# def game():
-#     """main"""
-#     print("Welcome to the guessing number game.\nI'm thinking of a number between 1 and 100.")
-#     number = random.randint(1, 100)
-#     dif = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
-#     if dif == "hard":
-#         check(number, 5)
-#     elif dif == "easy":
-#         check(number, 10)
-#     else:
-#         print("Wrong choice")
-#
-#
-# game()
-
 
 def check(guess_f, account_a_f, account_b_f):
     if account_a_f['follower_count'] > account_b_f['follower_count']:
